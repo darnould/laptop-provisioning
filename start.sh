f3b7b09804b1bb64c9246d10be68abb80a00b444
Dan Arnould <dan@arnould.co.uk>
2015-01-11 12:41:42 -0800

    Add start.sh
    Installs Homebrew and Ansible

---
+++ start.sh
@@ -0,0 +1,13 @@
+missing() {
+  [ -z $(command -v $1) ]
+}
+
+# Install Homebrew
+if missing "brew"; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install Ansible
+if missing "ansible"; then
+  brew install ansible
+fi
